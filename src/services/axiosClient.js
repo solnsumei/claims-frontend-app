ac5d2d81da01199881823485cd15eed483bfaf8d
solnsumei <solnsumei@gmail.com>
2021-08-09 21:34:42 +0100

    Change base url

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
-  // baseURL: 'http://167.99.91.68/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'http://167.99.91.68/api/v1',
 });
 
 Object.freeze(axiosClient);
